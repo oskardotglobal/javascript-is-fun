a6a243f17e57bfef0428be874cba1da6ff9114c3
Oskar Manhart <52569953+oskardotglobal@users.noreply.github.com>
2024-01-24 16:01:57 +0100

    state machine woo

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function setup() {
 }
 
 function draw() {
-    ManagerFactory.get().draw();
+    ManagerFactory.get().state.do();
 }
 
 function mousePressed() {
@@ -24,39 +24,5 @@ function windowResized() {
 }
 
 function keyPressed() {
-    const manager = ManagerFactory.get();
-    const player = manager.player;
-
-    /**
-     * @param {CIterator} iter
-     * @recursive
-     */
-    function jumpToLetter(iter) {
-        if (!iter.hasNext()) return;
-
-        /** @type {Letter} */
-        const letter = iter.next();
-
-        const timer = new Timer(
-            () => {
-                if (player.position.equals(letter.position)) return true;
-
-                const by = new Vec2D(1, 1);
-                const delta = letter.position.sub(player.position);
-                const normalized = delta.normalized();
-
-                if (delta.length() < 1 || !isFinite(normalized.x)) return true;
-
-                player.position = player.position.add(normalized.mult(by));
-                timer.reset();
-
-                return false;
-            },
-            60 / 1000,
-            true,
-            () => setTimeout(() => jumpToLetter(iter), 1000)
-        );
-    }
-
-    jumpToLetter(new CIterator(manager.elements));
+    ManagerFactory.get().state.enter(States.MOVE_TO_LETTER);
 }
